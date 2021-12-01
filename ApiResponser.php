44928bf338b6e33684856a03825006016bbec8e4
Tahmid Ferdous <62140352+tahmid-tf@users.noreply.github.com>
2021-12-01 12:48:04 +0600

    ApiResponser.php

---
+++ ApiResponser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Response;
+
+trait ApiResponser
+{
+    public function successResponse($data, $code = Response::HTTP_OK)
+    {
+        return \response()->json([
+            'data' => $data,
+            'status' => $code
+        ],$code);
+    }
+
+    public function errorResponse($message, $code)
+    {
+        return \response()->json([
+            'error' => $message,
+            'code' => $code
+        ],$code);
+    }
+}
